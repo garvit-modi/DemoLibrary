9a3e5a1ee65e07884b699335cd2578dd42125cae
garvit <garvit.kh05@gmail.com>
2022-04-06 15:11:21 +0530

    Initial commit

---
+++ app/src/main/java/com/example/demo/Logger.kt
@@ -0,0 +1,19 @@
+package com.example.demo
+
+import android.support.multidex.BuildConfig
+import android.util.Log
+
+object Logger {
+    fun d(key: String?, msg: String?) {
+        if (BuildConfig.DEBUG) {
+            Log.d(key, msg!!)
+        }
+    }
+
+    fun e(key: String?, msg: String?) {
+        if (BuildConfig.DEBUG) {
+            Log.e(key, msg!!)
+        }
+    }
+}
+
